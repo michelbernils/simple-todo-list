56d4d4756031cd46252c89498bfaf8be5743a9fd
Michel <bernils.michel@gmail.com>
2021-09-28 21:42:28 -0300

    fixes on toggle completion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,10 +30,10 @@ export function TaskList() {
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
 
-    const taskDone = tasks.map( task => (
-      task.id == id ? {
-        ...tasks, isComplete: !task.isComplete
-      } : task))
+    const taskDone = tasks.map( task => 
+      task.id === id ? {
+        ...task, isComplete: !task.isComplete
+      } : task)
   
       setTasks(taskDone)
   }
